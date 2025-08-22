cf17d782f83b288a2c67d89da2e5ebf0bee4ff5d
Chinmay Mahapatra <chinmaymahapatra6@gmail.com>
2025-08-22 17:33:00 +0530

    Updated

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+
+BACKEND_URL=""
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    form_data = dict(request.form)
+    try:
+        requests.post(BACKEND_URL + '/submittodoitem', json=form_data)
+        return "Data submitted successfully"
+    except Exception as e:
+        return e
+
 
 if __name__=='__main__':
     app.run(debug=True)
